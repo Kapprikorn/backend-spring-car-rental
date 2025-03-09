0e3d5f77cfb0e3b56adb33a54c744f3b6ac8adc8
Kapprikorn <122310945+Kapprikorn@users.noreply.github.com>
2025-03-09 10:06:53 +0100

    Creates a Spring Boot project

---
+++ src/test/java/nl/novi/sd/carpark/CarParkApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.sd.carpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
