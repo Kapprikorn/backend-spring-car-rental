3516489f5a3b61b01bb993c4dd01423cf92bd483
Kapprikorn <122310945+Kapprikorn@users.noreply.github.com>
2025-03-16 16:47:43 +0100

    created repositories for all base models

---
+++ src/main/java/nl/novi/sd/carrental/repository/ParkingLotRepository.java
@@ -0,0 +1,9 @@
+package nl.novi.sd.carrental.repository;
+
+import nl.novi.sd.carrental.model.ParkingLot;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingLotRepository extends JpaRepository<ParkingLot, Long> {
+}
