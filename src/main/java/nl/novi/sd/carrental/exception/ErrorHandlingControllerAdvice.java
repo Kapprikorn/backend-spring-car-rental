2abeca9fa76aea9a13f64008aef2ef86da66f213
Kapprikorn <122310945+Kapprikorn@users.noreply.github.com>
2025-04-05 14:18:54 +0200

    Adds basic exception handlers for validation and not found errors
    Introduced handling for `ConstraintViolationException` and `ResourceNotFoundException` in `ErrorHandlingControllerAdvice`.

--- src/main/java/nl/novi/sd/carrental/exception/ErrorHandlingControllerAdvice.java
+++ src/main/java/nl/novi/sd/carrental/exception/ErrorHandlingControllerAdvice.java
@@ -1,4 +1,26 @@
 package nl.novi.sd.carrental.exception;
 
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class ErrorHandlingControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    @ExceptionHandler(ConstraintViolationException.class)
+    public String handleConstraintViolationException(ConstraintViolationException ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public String handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ex.getMessage();
+    }
 }
